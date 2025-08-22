836cb540347bde449ed5db09ff735a5f9c415a58
piperpei <piperpei@yahoo.com>
2025-08-21 18:27:57 -0700

    Adding Burgers and Updating Featured Drink

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your solution in this file!
+// Global Scope Variables
+
+let burgers = ['Hamburger', 'Cheeseburger'];
+let featuredDrink = 'Strawberry Milkshake'; 
+
+// Adding a New Burger
+
+function addBurger() {
+    let newBurger = 'Flatburger'; 
+    // Adding to Array 
+    burgers.push(newBurger); 
+}
+
+addBurger();
+
+if (true) {
+    // Block Scope and Push 
+    let anothernewBurger = 'Maple Bacon Burger'; 
+    burgers.push(anothernewBurger);
+
+    // Change Featured Drink
+    function changeFeaturedDrink() {
+        featuredDrink = 'The JavaShake';
+    }
+
+    changeFeaturedDrink(); 
+
+}
+
+console.log(burgers);
+console.log(featuredDrink); 
